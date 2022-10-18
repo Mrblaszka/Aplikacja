2922daee57bfce9e92effa02eca3aa30264d85f1
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-10-18 10:03:58 +0200

    Ready look

--- app/src/main/java/com/example/aplikacja/MainActivity.kt
+++ app/src/main/java/com/example/aplikacja/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.aplikacja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    var text1 = findViewById<EditText>(R.id.txt1)
 }
